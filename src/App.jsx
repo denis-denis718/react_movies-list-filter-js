9898e9349ee90cef9a50adfc70163fccbfeabc34
Denis <denchick1546450@gmail.com>
2025-09-17 19:32:47 +0300

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,64 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+
+export const App = () => {
+
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const normalizedQuery = query.trim().toLowerCase();
+
+    return (
+      movie.title.toLowerCase().includes(normalizedQuery) ||
+      movie.description.toLowerCase().includes(normalizedQuery)
+    )
+  }
+  )
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            {/* <button
+              onClick={() => {
+                setQuery('');
+              }}
+              >
+                Reset
+              </button> */}
+            {/* При желании, можно добавить кнопку, Reset, которая будет чистить поиск */}
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={(e) => {
+                  setQuery(e.currentTarget.value)
+                }}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList
+          movies={visibleMovies}
+        />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  )
+};
